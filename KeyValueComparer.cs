37ab34ef0506478effc3a945dcfe59dca981c73a
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2023-04-02 14:46:30 -0400

    Term Frequency

---
+++ KeyValueComparer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSSearchEngine
+{
+    public class KeyValueComparer : IComparer<KeyValuePair<string, int>>
+    {
+        public int Compare(KeyValuePair<string, int> x, KeyValuePair<string, int> y)
+        {
+            return x.Value == y.Value ? 0 : x.Value > y.Value ? -11 : 1;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,24 +1,39 @@
-﻿namespace CSSearchEngine
+﻿using HtmlAgilityPack;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+
+namespace CSSearchEngine
 {
     public class Program
     {
         private const string strDirectory = "D:\\HTML-REZ";
-
         static void Main(string[] args)
         {
-            List<string> filesList = GetHtmlFiles();
-            Dictionary<string, Dictionary<string, ulong>> DocsFT 
-                        = new Dictionary<string, Dictionary<string, ulong>>();
+            List<string> filesList = GetHtmlFilesPaths();
+            Dictionary<string, Dictionary<string, int>> DocsFT 
+                        = new Dictionary<string, Dictionary<string, int>>();
 
             foreach (var filePath in filesList)
             {
                 string strFileName = Path.GetFileNameWithoutExtension(filePath);
-                DocsFT.Add(strFileName, CreateFrequenceTable(filePath));
-            }
+                var ft = CreateFrequenceTable(filePath);
+                var sft = SortFrequencyTableByFrequency(ft);
+                
+                if (sft.Count() == 0)
+                    continue;
 
+                Console.WriteLine($"{strFileName}");
+                foreach (KeyValuePair<string, int> kvp in sft)
+                    Console.WriteLine($"\t{kvp.Key}: {kvp.Value}");
+                
+
+                Console.WriteLine("");
+            }
         }
 
-        public static List<string> GetHtmlFiles()
+        public static List<string> GetHtmlFilesPaths()
         {
             List<string> files =
                 Directory.GetFiles(strDirectory, "*.*", SearchOption.AllDirectories)
@@ -26,19 +41,49 @@
                          .ToList();
             return files;
         }
-
         public static List<string> GetTokens(string strFilePath)
         {
-            return File.ReadAllText(strFilePath).Split(" ",
+            string strFileContent = File.ReadAllText(strFilePath);
+            return  ExtractText(strFileContent).Split(" ",
                                 StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
                                 .Select(x => x.ToUpper())
-                                .Where( x => x.Length > 1)
+                                .Where(TokenCriterias())
                                 .ToList();
         }
-
-        public static Dictionary<string,ulong> CreateFrequenceTable(string strFilePath)
+        public static Func<string,bool> TokenCriterias()
         {
-            Dictionary<string, ulong> freqTable = new  Dictionary<string, ulong>();
+            Func<string, bool> criList = x => ( x.Length > 3 && x.Length < 15);
+            return criList;
+        }
+        public static Func<string, string> TokenSelectors()
+        {
+            Func<string, string> criList = x => x.ToUpper() ;
+            Func<string, string> criList2 = (input) =>
+            {
+                if (input.EndsWith("."))
+                {
+                    input = input.Substring(0, input.Length - 1);
+                    return input;
+                }
+                return input;
+            };
+            Func<string, string> criList3 = (input) =>
+            {
+                if (input.StartsWith("."))
+                {
+                    input = input.Substring(1, input.Length);
+                    return input;
+                }
+                return input;
+            };
+
+
+
+            return criList;
+        }
+        public static Dictionary<string,int> CreateFrequenceTable(string strFilePath)
+        {
+            Dictionary<string, int> freqTable = new  Dictionary<string, int>();
             List<string> tokenz = GetTokens(strFilePath);
             foreach (var term in tokenz)
             {
@@ -47,7 +92,44 @@
                 else
                     freqTable.Add(term, 1);                
             }
+
+
             return freqTable;
+        }
+        public static Dictionary<string, int> SortFrequencyTableByFrequency(Dictionary<string, int> FrequencyTable)
+        {
+            Dictionary<string, int> freqTable = new Dictionary<string, int>();
+            List<KeyValuePair<string, int>> list = FrequencyTable.ToList();
+            list.Sort(new  KeyValueComparer());
+            foreach (var item in list)
+            {
+                freqTable.Add(item.Key, item.Value);
+            }
+            return freqTable;
+        }
+        public static string ExtractText(string html)
+        {
+            if (html == null)
+            {
+                throw new ArgumentNullException("html");
+            }
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            var chunks = new List<string>();
+
+            foreach (var item in doc.DocumentNode.DescendantNodesAndSelf())
+            {
+                if (item.NodeType == HtmlNodeType.Text)
+                {
+                    if (item.InnerText.Trim() != "")
+                    {
+                        chunks.Add(item.InnerText.Trim());
+                    }
+                }
+            }
+            return String.Join(" ", chunks);
         }
     }
 }
